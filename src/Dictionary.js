e6f1b58e77eabe3ecb87e781fe20a5a6689b5c84
natkaulaP <89267067+natkaulaP@users.noreply.github.com>
2022-02-02 09:42:09 +0100

    addinga functions

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Dictionary.css'
 export default function Dictionary(){
+  const [keyword, setKeyword]=useState("");
+  function search(event){
+    event.preventDefault();
+    alert(`Searching for ${keyword} definition`);
+  }
+  function updateSearch(event){
+setKeyword(event.target.value);
+
+  }
   return (
     <div className="Dictionary card">
       <h1 className="Dictionary-title cyan">DICTIONARY</h1>
       <h2 className="Dictionary-question">
         What word definition you wanna search for?
       </h2>
+       <form onSubmit={search}>
       <input
-        className="Dictionary-description d-inline-flex"
-        type="text"
+        className="Dictionary-description "
+        type="search"
         placeholder="Type a word"
         autoComplete="off"
         autoFocus="on"
+        onChange={updateSearch}
       />
+      </form>
     </div>
   );
 }
