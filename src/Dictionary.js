dba033833dba099fb03a9ed28dbd73465458ab65
natkaulaP <89267067+natkaulaP@users.noreply.github.com>
2022-02-02 10:14:01 +0100

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,17 @@
 import React, {useState} from 'react';
 import './Dictionary.css'
+import axios from 'axios';
 export default function Dictionary(){
   const [keyword, setKeyword]=useState("");
+
+function handleResponse(response) {
+  console.log(response.data[0])
+}
+
   function search(event){
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then (handleResponse);
   }
   function updateSearch(event){
 setKeyword(event.target.value);
